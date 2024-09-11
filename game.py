45159d8f6522833c444a2a44ba8c0e6582d226ec
Jakub Vonášek <jkb.vonasek@gmail.com>
2024-09-12 01:11:39 +0800

    Add collisions and jumping physics

--- game.py
+++ game.py
@@ -44,9 +44,10 @@ class Game:
 
             self.tilemap.render(self.surface)
 
-            self.player.update((self.movement[1] - self.movement[0],0))
+            self.player.update(self.tilemap, (self.movement[1] - self.movement[0],0))
             self.player.render(self.surface)
 
+            print(self.tilemap.physics_rects_around(self.player.pos))
 
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
@@ -57,6 +58,8 @@ class Game:
                         self.movement[0] = True
                     if event.key == pygame.K_RIGHT:
                         self.movement[1] = True
+                    if event.key == pygame.K_UP:
+                        self.player.velocity[1] = -3
                 if event.type == pygame.KEYUP:
                     if event.key == pygame.K_LEFT:
                         self.movement[0] = False
